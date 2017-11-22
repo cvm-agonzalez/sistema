b9a3bb0847bf3567078c6383c566aed493f018be
Adrian Gonzalez <agonzalez@agonzalez.nb.sis.cooperativaobrera.coop>
2017-11-21 23:39:40 -0300

    Agrego funcion para buscar cupones deprecated

--- application/models/socios_model.php
+++ application/models/socios_model.php
@@ -97,12 +97,10 @@ class Socios_model extends CI_Model {
     }
 
     public function update_socio($id,$data){
-	if ( $categ = $this->get_cat($id, $data) ) {
-		var_dump($categ);
-		if ( $categ->id_ant != $data['categoria'] ) {
-        		$this->load->model('pagos_model');
-	                $this->pagos_model->registrar_pago('debe',$id,0.00,'Cambio de Categoria de '.$categ->descr_ant.' a '.$categ->descr_new);
-		}
+	$categ = $this->get_cat($id, $data);
+	if ( $categ['cambio'] == 1 ) {
+        	$this->load->model('pagos_model');
+		$this->pagos_model->registrar_pago('debe',$id,0.00,'Cambio de Categoria de '.$categ['descr_ant'].' a '.$categ['descr_new'],0,0);
 	}
         $this->db->where('Id', $id);
         $this->db->update('socios', $data); 
@@ -110,17 +108,18 @@ class Socios_model extends CI_Model {
 
     public function get_cat($id, $data){
 	$cat_new=$data['categoria'];
-	$query="SELECT s.categoria id_ant, c1.nomb descr_ant, $cat_new id_new, c2.nomb descr_new
+	$query="SELECT s.categoria id_ant, c1.nomb descr_ant, $cat_new id_new, c2.nomb descr_new, IF ( $cat_new != s.categoria , 1, 0) cambio
 		FROM socios s
-			JOIN categorias c1 ON ( s.categoria = c1.Id )
-			JOIN categorias c2 ON ( $cat_new = c2.Id )
+			LEFT JOIN categorias c1 ON ( s.categoria = c1.Id )
+			LEFT JOIN categorias c2 ON ( $cat_new = c2.Id )
 		WHERE s.Id = $id; ";
-        $result = $this->db->query($query)->result(); 
+        $result = $this->db->query($query)->row(); 
         if ( $result ) {
-	    $cat = array( 'id_ant' => $result['id_ant'],
-			'descr_ant' => $result['descr_ant'],
-			'id_new' => $result['id_new'],
-			'descr_new' => $result['descr_new']
+	    $cat = array( 'id_ant' => $result->id_ant,
+			'descr_ant' => $result->descr_ant,
+			'id_new' => $result->id_new,
+			'descr_new' => $result->descr_new,
+			'cambio' => $result->cambio
 		);
             return $cat;
         }else{
@@ -183,6 +182,9 @@ class Socios_model extends CI_Model {
     public function actualizar_menor($id_menor){
         $this->db->where('Id',$id_menor);
         $this->db->update('socios',array('tutor'=>'0','categoria'=>'2'));
+	$this->load->model('pagos_model');
+	$this->pagos_model->registrar_pago('debe',$id_menor,0.00,'Cambio de Categoria de Menor a Mayor - Proceso Facturacion',0,0);
+
     }
     public function get_socios_pagan($facturado=false){
         if($facturado){
