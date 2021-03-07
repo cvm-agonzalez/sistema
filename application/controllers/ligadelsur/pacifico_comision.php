3f1ab45a537886bf7e3d31a11647166cdfc28775
root <root@agonzalez.nb.sis.cooperativaobrera.coop>
2021-03-06 23:46:23 -0300

    Incorporo accesos de comisiones

---
+++ application/controllers/ligadelsur/pacifico_comision.php
@@ -0,0 +1,33 @@
+<?
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+ 
+class pacifico_comision extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+	
+        	$this->load->model('login_model');
+        	$this->load->library(array('session','form_validation'));
+        	$this->load->helper(array('url','form'));
+        	$this->load->database('default');        
+        	$data['id_entidad'] = 7;
+		$this->load->model('general_model');
+		$entidad = $this->general_model->get_ent_dir($data['id_entidad']);
+
+		$data['ent_abreviatura'] = $entidad->abreviatura;
+		$data['ent_nombre'] = $entidad->descripcion;
+		$data['ent_directorio'] = $entidad->dir_name;
+
+		$this->session->set_userdata($data);
+
+        	redirect(base_url().'comisiones');
+
+
+    }
+    
+
+
+}
+?>
---
+++ application/controllers/ligadelsur/rosario_comision.php
@@ -0,0 +1,33 @@
+<?
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+ 
+class rosario_comision extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+	
+        	$this->load->model('login_model');
+        	$this->load->library(array('session','form_validation'));
+        	$this->load->helper(array('url','form'));
+        	$this->load->database('default');        
+        	$data['id_entidad'] = 3;
+		$this->load->model('general_model');
+		$entidad = $this->general_model->get_ent_dir($data['id_entidad']);
+
+		$data['ent_abreviatura'] = $entidad->abreviatura;
+		$data['ent_nombre'] = $entidad->descripcion;
+		$data['ent_directorio'] = $entidad->dir_name;
+
+		$this->session->set_userdata($data);
+
+        	redirect(base_url().'comisiones');
+
+
+    }
+    
+
+
+}
+?>
